5ea667602fe0cd95b39f98529fa848f25f508315
nabinsademba <nabinsademba@gmail.com>
2018-03-07 11:19:19 +0545

    adding insert and select

---
+++ select.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP</title>
+</head>
+<body>
+	
+	<ul>
+	<?php  
+
+	$db = mysqli_connect('localhost', 'root', '', 'php');
+	$sql = "SELECT * FROM users";
+	$result = mysqli_query($db, $sql);
+	foreach ($result as $row) {
+		printf('<li><span style="color: %s;">%s (%s)</span></li>',
+			htmlspecialchars($row['color']),
+			htmlspecialchars($row['name']),
+			htmlspecialchars($row['gender'])
+		);
+	}
+
+	mysqli_close($db);
+
+
+	?>
+	</ul>
+
+
+</body>
+</html>
