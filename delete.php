4b99d7331c1f34341f9c8835f288bb86890a918a
nabinsademba <nabinsademba@gmail.com>
2018-03-11 19:47:48 +0545

    all set and finish

--- delete.php
+++ delete.php
@@ -1,27 +1,28 @@
-<?php 
-	
-	if (isset($_GET['id']) && ctype_digit($_GET['id'])){
-		$id = $_GET['id'];
-	} else {
-		header('location: select.php');
-	}
+<?php
+require 'auth.php';
 
- ?>
+if (isset($_GET['id']) && ctype_digit($_GET['id'])) {
+	$id = $_GET['id'];
+} else {
+	header('location: select.php');
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>PHP</title>
 </head>
 <body>
-	<?php 
-		 readfile('navigation.tmpl.html');
+	<?php
+readfile('navigation.tmpl.html');
 
-		$db = mysqli_connect('localhost', 'root', '', 'php');
-		$sql = "DELETE FROM users WHERE id =$id";
-		mysqli_query($db, $sql);
-		echo '<p>User deleted.</p>';
-		mysqli_close($db);
+$db = mysqli_connect('localhost', 'root', '', 'php');
+$sql = "DELETE FROM users WHERE id =$id";
+mysqli_query($db, $sql);
+echo '<p>User deleted.</p>';
+mysqli_close($db);
 
-	 ?>
+?>
 </body>
 </html>
